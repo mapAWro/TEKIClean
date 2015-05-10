100a45f51bcb1723ff20c7a11ed1508864f6aea6
Doomsay <sjazwiec@interia.pl>
2015-05-10 21:48:21 +0200

    New_Android
    Android - migracja jdbc/json

---
+++ Mobile/android_login_api/include/config.php
@@ -0,0 +1,11 @@
+﻿Config.php
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "zigadmin");
+define("DB_PASSWORD", "dms");
+define("DB_DATABASE", "android_api");
+?>
---
+++ Mobile/readme.txt
@@ -0,0 +1,3 @@
+1. install WAMP
+2. paste android_login_api into /WAMP_INSTALLATION_DIR/www
+3. ZIG-TEST -> Eclipse workspace
